50af2cf0d6808fc5a6054005eeb84f117ab82fee
Abilat01 <Abilat01@yandex.ru>
2021-08-24 12:20:26 +0300

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Danya on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
