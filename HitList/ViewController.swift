a0c8dde00cb2178626b14c8c3872307083845e51
Abilat01 <Abilat01@yandex.ru>
2021-08-24 16:11:33 +0300

    CoreData model

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func NewTaskSave(_ sender: UIBarButtonItem) {
+        
+        let alertControler = UIAlertController(title: "Новое имя", message: "Добавте новое имя", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Сохранить", style: .default) { [unowned self] (action) in
+            guard let textField = alertControler.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        alertControler.addTextField()
+        
+        alertControler.addAction(saveAction)
+        alertControler.addAction(cancelAction)
+        
+        present(alertControler, animated: true, completion: nil)
+    }
+    
+}
 
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    
 }
 
