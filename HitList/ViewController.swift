55b323a3cfb224d1b5d857f7c000118c055bd8a0
Abilat01 <Abilat01@yandex.ru>
2021-08-25 16:26:26 +0300

    Save

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managerContext  = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managerContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     
@@ -26,7 +41,7 @@ class ViewController: UIViewController {
             guard let textField = alertControler.textFields?.first, let nameToSave = textField.text else {
                 return
             }
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
@@ -38,6 +53,25 @@ class ViewController: UIViewController {
         present(alertControler, animated: true, completion: nil)
     }
     
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managerContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managerContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managerContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managerContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
@@ -47,13 +81,15 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         
         return cell
     }
